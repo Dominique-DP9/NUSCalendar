4faf7f07e28ba331236f0a33205628b67f4c3afc
Dominique-DP9 <e1482079@u.nus.edu>
2025-02-22 17:11:12 +0800

    Major milestones: API w database, EventCard semi designed

--- NUSCalendy/src/main.tsx
+++ NUSCalendy/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import './index.css'
+// import App from './App.tsx'
+import Calendar from './components/Calendar/Calendar.tsx'
+// import EventCard from './components/EventCard/EventCard.tsx'
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Calendar />
+    
   </StrictMode>,
 )

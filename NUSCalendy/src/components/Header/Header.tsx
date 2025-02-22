c50f656d00fdd16cfbcb14c164c219c8e88bc612
Minh <ngohoangnhatminh@gmail.com>
2025-02-22 22:47:25 +0800

    feat: import mui

--- NUSCalendy/src/components/Header/Header.tsx
+++ NUSCalendy/src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Header.css'
+import { Avatar } from '@mui/material'
 
 export const Header = () => {
     return (
         <>
             <div className="header">
-                NUSCalendy
+                {/* <div className="logo">Logo</div> */}
+                <Avatar>Logo</Avatar>
+                <div className="title">
+                    <h1>NUSCalendy</h1>
+                </div>
+                <div className="menu">Menu</div>
             </div>
         </>
     )

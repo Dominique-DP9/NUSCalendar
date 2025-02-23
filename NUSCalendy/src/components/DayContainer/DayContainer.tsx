8924e008fe51798dd1262882b358b872c4694068
Mai <truongkimmai2004@nguyendudalat.edu.vn>
2025-02-23 11:21:06 +0800

    change font

--- NUSCalendy/src/components/DayContainer/DayContainer.tsx
+++ NUSCalendy/src/components/DayContainer/DayContainer.tsx
@@ -1,53 +1,69 @@
 //import React from 'react'
+import { useState } from 'react'
 import "./DayContainer.css"
 import { EventProp } from "../Calendar/Calendar.tsx"
 import { formatDay } from "../../utilities/formatDay.ts"
+import EventCard from '../EventCard/EventCard.tsx'
 
 const DayContainer = ({ events, times }: { events: EventProp[], times: string[] }) => {
-    // const handleButtonClick = (task) => {
-    //     const temp = 0
-    // }
-    const Events = [
-        {
-            id: 1,
-            date: "Monday",
-            name: "Event",
-            details: "Description",
-        },
-        {
-            id: 2,
-            date: "Tuesday",
-            name: "Event",
-            details: "Description",
-        },
-        {
-            id: 3,
-            date: "Tuesday",
-            name: "Event",
-            details: "Description",
-        },
-        {
-            id: 4,
-            date: "Tuesday",
-            name: "Event",
-            details: "Description",
-        },
-    ]
+    const emptyEvent : EventProp = {
+        id: 0,
+        title: "",
+        time: "",
+        description: "",
+    }
+    const [eventCard, setEventCard] = useState<EventProp>(emptyEvent)
+    const [eCardVisible, setECardVisible] = useState<boolean>(false)
+
+    const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+    const handleButtonClick = (event: EventProp) => {
+        setEventCard(event)
+        setECardVisible(!eCardVisible)
+    }
+    // const Events = [
+    //     {
+    //         id: 1,
+    //         date: "Monday",
+    //         name: "Event",
+    //         details: "Description",
+    //     },
+    //     {
+    //         id: 2,
+    //         date: "Tuesday",
+    //         name: "Event",
+    //         details: "Description",
+    //     },
+    //     {
+    //         id: 3,
+    //         date: "Tuesday",
+    //         name: "Event",
+    //         details: "Description",
+    //     },
+    //     {
+    //         id: 4,
+    //         date: "Tuesday",
+    //         name: "Event",
+    //         details: "Description",
+    //     },
+    // ]
 
     return <>
     {
         times.map((time) => {
             const day = new Date(time).getDay()
+            const date = new Date(time).getDate()
+            const month = new Date(time).getMonth()
             console.log(day)
             return <>
                 <div className="day-container">
-                    <div className="day-title">{formatDay(day)}</div>
+                    <div className="day-title">{formatDay(day)}, {date} {months[month]}</div>
                     <div className="event-container">
                         {
                             events
                                 .filter((event) => event.time == time)
                                 .map((event) => {
-                                    return <button className="event" onClick={() => { }}>
+                                    return <button className="event" onClick={() => {handleButtonClick(event)}}>
                                         <div className="event-title">{event.title}</div>
                                         <div className="event-description">{event.description}</div>
                                     </button>
@@ -57,6 +73,17 @@ const DayContainer = ({ events, times }: { events: EventProp[], times: string[] 
                 </div>
             </>
         })
+    }
+    {eCardVisible &&
+        <EventCard
+        id = {eventCard?.id}
+        title = {eventCard?.title}
+        author = {eventCard?.title}
+        eventDate = {eventCard?.time}
+        createdDate = {eventCard?.time}
+        img = {eventCard?.title}
+        desc = {eventCard?.description}
+    />
     }
     </>
 }
--- NUSCalendy/src/components/EventCard/EventCard.tsx
+++ NUSCalendy/src/components/EventCard/EventCard.tsx
@@ -8,9 +8,9 @@ type Event = {
   title: string,
   author: string,
   eventDate: string,
-  createdDate: string, 
+  createdDate: string,
   img: string,
-  desc: string
+  desc: string,
 }
 
 const EventCard: React.FC<Event> = ({
@@ -24,36 +24,36 @@ const EventCard: React.FC<Event> = ({
 }) => {
 
   return (
-    <div className="card-container"> 
-      {/* Card body */}
-      <div className="card">
-        {/* Card Image */ }
-        <div className="cardHeader"> 
-          <img src={img} className="cardImg" alt={`Image of ${title}`} />
-          <div className="notion-icon">
-            <svg width="40" height="40" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M12 2L15 8H21L17 12L19 18L12 14L5 18L7 12L3 8H9L12 2Z"/>
-            </svg>
+    <div className ="card-bg">
+      <div className="card-container" >
+        {/* Card body */}
+        <div className="card">
+          {/* Card Image */}
+          <div className="cardHeader">
+            <img src={img} className="cardImg" alt={`Image of ${title}`} />
+            <div className="notion-icon">
+              <svg width="40" height="40" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M12 2L15 8H21L17 12L19 18L12 14L5 18L7 12L3 8H9L12 2Z" />
+              </svg>
+            </div>
           </div>
-        </div>
 
-        {/* Card Details */}
-        <div className="cardContents">
-          <h2 className="cardTitle">{title} // Todo: Style both the title and description texts</h2> 
-          <h4 className="cardMeta"><i> Event date: </i> {eventDate}  </h4>
-          <hr></hr>
-          <h4 className='cardDesc'>{desc}</h4>
-          <div className='cardMeta'>
-            <span><strong>Creator: {author} </strong>| </span>
-            
-            <span><i> Date created:</i> {createdDate} | </span>
-            <span><i> Event Number: #</i> {id.toString()}</span>
-            {/* <p>{id.toString()}</p> */}
+          {/* Card Details */}
+          <div className="cardContents">
+            <h2 className="cardTitle">{title} // Todo: Style both the title and description texts</h2>
+            <h4 className="cardMeta"><i> Event date: </i> {eventDate}  </h4>
+            <hr></hr>
+            <h4 className='cardDesc'>{desc}</h4>
+            <div className='cardMeta'>
+              <span><strong>Creator: {author} </strong>| </span>
+
+              <span><i> Date created:</i> {createdDate} | </span>
+              <span><i> Event Number: #</i> {id.toString()}</span>
+              {/* <p>{id.toString()}</p> */}
+            </div>
           </div>
         </div>
       </div>
-      
-      
     </div>
   )
 }

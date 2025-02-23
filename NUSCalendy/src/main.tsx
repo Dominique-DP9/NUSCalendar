372d015464c4b4ce31584b93ffec3c12e4305b8a
Dominique-DP9 <e1482079@u.nus.edu>
2025-02-23 12:41:42 +0800

    calendar

--- NUSCalendy/src/main.tsx
+++ NUSCalendy/src/main.tsx
@@ -2,25 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.tsx'
-// import Calendar from './components/Calendar/Calendar.tsx'
-import EventCard from './components/EventCard/EventCard.tsx'
+import Calendar from './components/Calendar/Calendar.tsx'
+// import EventCard from './components/EventCard/EventCard.tsx'
 
 
-const event = {
-    id: 1,
-    title: "Residance!!!!!!!",
-    author: "Domz",
-    eventDate: "2025-03-10",
-    createdDate: "2025-02-20",
-    img: "https://thumb.photo-ac.com/3e/3ee6943610e29f5b124192e73d1a3418_t.jpeg",
-    desc: "I love dancing!!!",
-  }
+// const event = {
+//     id: 1,
+//     title: "Residance!!!!!!!",
+//     author: "Domz",
+//     eventDate: "2025-03-10",
+//     createdDate: "2025-02-20",
+//     img: "https://thumb.photo-ac.com/3e/3ee6943610e29f5b124192e73d1a3418_t.jpeg",
+//     desc: "I love dancing!!!",
+//   }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <Calendar /> */}
-    <EventCard {...event} />
+    <Calendar />
+    {/* <EventCard {...event} /> */}
     
   </StrictMode>,
 )

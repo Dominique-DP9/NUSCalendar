a1d45d765e4e7cd067e4baa5fa4ae0d8bb0eb51e
Minh <ngohoangnhatminh@gmail.com>
2025-02-23 12:22:58 +0800

    feat: add "Add Event" option and "Profile Menu" to header

--- NUSCalendy/src/components/Header/Header.tsx
+++ NUSCalendy/src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
 import './Header.css'
+import { AccountMenu } from './AccountMenu'
+import { Button } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
 
 export const Header = () => {
     return (
         <>
             <div className="header">
-                NUSCalendy
+                <div className='logo'>LOGO</div>
+                <div className="title">
+                    <h1>NUSCalendy</h1>
+                </div>
+                <div className='menu'>
+                    <Button><AddIcon/> Add Event</Button>
+                    <AccountMenu></AccountMenu>
+                </div>
+                
             </div>
         </>
     )
